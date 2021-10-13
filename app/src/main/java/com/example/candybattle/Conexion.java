c180d121293160421b455eae1ea1aceae646fe45
LinaPeralta <69905738+LinaPeralta@users.noreply.github.com>
2021-10-12 21:28:42 -0500

    Pantallas agregadas

---
+++ app/src/main/java/com/example/candybattle/Conexion.java
@@ -0,0 +1,33 @@
+package com.example.candybattle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Conexion extends AppCompatActivity {
+
+    private Button conectarBtn;
+    private EditText nombreTxt;
+    private EditText ipTxt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conexion);
+
+        conectarBtn = findViewById(R.id.conectarBtn);
+        nombreTxt = findViewById(R.id.nombreTxt);
+        ipTxt = findViewById(R.id.ipTxt);
+
+
+
+        conectarBtn.setOnClickListener(
+                (v) -> {
+
+
+
+                });
+    }
+}
--- app/src/main/java/com/example/candybattle/Instrucciones.java
+++ app/src/main/java/com/example/candybattle/Instrucciones.java
@@ -2,26 +2,13 @@ package com.example.candybattle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
-
-
+public class Instrucciones extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
-
-
-
+        setContentView(R.layout.activity_instrucciones);
     }
-
-
-
 }
