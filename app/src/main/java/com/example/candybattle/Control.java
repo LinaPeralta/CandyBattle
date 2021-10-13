8d4a97c973be641e547514bd3e54c27781f98a7b
LinaPeralta <69905738+LinaPeralta@users.noreply.github.com>
2021-10-12 19:29:55 -0500

    parte grafica de las tres pantallas

--- app/src/main/java/com/example/candybattle/Control.java
+++ app/src/main/java/com/example/candybattle/Control.java
@@ -1,4 +1,14 @@
 package com.example.candybattle;
 
-public class Control {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Control extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_control);
+    }
 }
